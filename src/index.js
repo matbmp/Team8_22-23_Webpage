cf4307750d41349647cdd11e12c7576d3b913c00
matbmp <mbiernacki531@gmail.com>
2022-11-25 23:23:38 +0100

    Poprawka na responsywność

--- src/index.js
+++ src/index.js
@@ -1,46 +1,45 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "./style.css";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Team from './team';
-import { Container, Navbar, Nav } from 'react-bootstrap';
-import Schedule from './schedule';
-import Progress from './progress';
-import Raports from './raports';
-import Docs from './docs';
+import Team from "./team";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import Schedule from "./schedule";
+import Progress from "./progress";
+import Raports from "./raports";
+import Docs from "./docs";
 
 class App extends Component {
   render() {
     return (
-        <div style={{background: `linear-gradient(white, lightgray)`}}>
-          <Navbar bg='primary' expand='lg' className="p-3">
-              <Navbar.Brand>
-                Programowanie zespołowe - zespół 8
-              </Navbar.Brand>
-              <Navbar.Collapse>
-                <Nav className="ms-auto">
-                <Nav.Link href="#home">Zespół</Nav.Link>
-                <Nav.Link href="#link">Harmonogram</Nav.Link>
-                <Nav.Link href="#link">Przebieg</Nav.Link>
-                <Nav.Link href="#link">Raporty</Nav.Link>
-                <Nav.Link href="#link">Dokumentacja</Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-          </Navbar>
-          <Team></Team>
-          <hr/>
-          <Schedule></Schedule>
-          <hr/>
-          <Progress></Progress>
-          <hr/>
-          <Raports></Raports>
-          <hr/>
-          <Docs></Docs>
-        </div>
+      <div style={{ background: `linear-gradient(white, lightgray)` }}>
+        <Navbar bg="primary" expand="lg" className="p-3">
+          <Navbar.Brand>Programowanie zespołowe - zespół 8</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home">Zespół</Nav.Link>
+              <Nav.Link href="#link">Harmonogram</Nav.Link>
+              <Nav.Link href="#link">Przebieg</Nav.Link>
+              <Nav.Link href="#link">Raporty</Nav.Link>
+              <Nav.Link href="#link">Dokumentacja</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Team></Team>
+        <hr />
+        <Schedule></Schedule>
+        <hr />
+        <Progress></Progress>
+        <hr />
+        <Raports></Raports>
+        <hr />
+        <Docs></Docs>
+      </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
--- src/timeline.js
+++ src/timeline.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Card, Row, Col } from "react-bootstrap";
+import { Card, Row, Col } from "react-bootstrap";
 
 class Circle extends React.Component {
   render() {
@@ -7,7 +7,7 @@ class Circle extends React.Component {
       position: "absolute",
       backgroundColor: this.props.color,
       borderWidth: this.props.borderWidth,
-      borderStyle: 'solid',
+      borderStyle: "solid",
       borderColor: this.props.borderColor,
       borderRadius: "50%",
       width: this.props.size,
@@ -32,54 +32,53 @@ class Rectangle extends React.Component {
   }
 }
 
-
 class TimeLine extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        events: [],
-      };
-    }
-    render() {
-  
-      let evs = this.props.events.map((obj, index) =>{
-          return (
-              <Row>
-            <Col md={1}>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: 'center',
-                  position: "relative",
-                  height: "100%",
-                }}
-              >
-                  <Rectangle color="#393E41" width={10}></Rectangle>
-                <Circle color="#FFFFFF" borderColor="#393E41" size={40} borderWidth={7}/>
-              </div>
-            </Col>
-            <Col md={5}>
-              <Card className="m-4">
-                <Card.Body>
-                  <Card.Title>{obj.title}</Card.Title>
-                  <Card.Text>
-                    <p className="text-muted">{obj.date}</p>
-                    <p>{obj.text}</p>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-          )
-      });
-  
-      return (
-        <>
-        {evs}
-        </>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+    };
   }
-  
-  export default TimeLine;
+  render() {
+    let evs = this.props.events.map((obj, index) => {
+      return (
+        <Row>
+          <Col xs={2} md={1}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                position: "relative",
+                height: "100%",
+              }}
+            >
+              <Rectangle color="#393E41" width={10}></Rectangle>
+              <Circle
+                color="#FFFFFF"
+                borderColor="#393E41"
+                size={40}
+                borderWidth={7}
+              />
+            </div>
+          </Col>
+          <Col xs={10} xl={8}>
+            <Card className="m-4">
+              <Card.Body>
+                <Card.Title>{obj.title}</Card.Title>
+                <Card.Text>
+                  <p className="text-muted">{obj.date}</p>
+                  <p>{obj.text}</p>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      );
+    });
+
+    return <>{evs}</>;
+  }
+}
+
+export default TimeLine;
